2375d9f917d1b8a4ebd55ab94d535f78909a6165
g1alexander <alexlds26@gmail.com>
2021-07-05 19:56:35 -0500

    fix: map issues

--- components/Mapa.vue
+++ components/Mapa.vue
@@ -1,41 +1,39 @@
 <template>
-<div id="map-wrap" style="height: 68vh">
- <client-only v-if="ip">
-   <l-map :zoom=15 :center="coordenadas()">
-     <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-      <l-marker :lat-lng="coordenadas()">
-      <l-popup>Aquí estoy</l-popup>
-     </l-marker>
-   </l-map>
-   
- </client-only>
-          
-</div>
-
+  <div id="map-wrap" style="height: 68vh">
+    <client-only>
+      <l-map :zoom="15" :center="coordenadas()">
+        <l-tile-layer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        ></l-tile-layer>
+        <l-marker :lat-lng="coordenadas()">
+          <l-popup>Your IP is here</l-popup>
+        </l-marker>
+      </l-map>
+    </client-only>
+  </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 export default {
-    name: 'Mapa',
-  data () {
-    return {
-    }
+  name: "Mapa",
+  data() {
+    return {};
   },
-  computed:{
-      ...mapState(["ip","loader"])
-
+  computed: {
+    ...mapState(["ip", "loader"])
   },
-  methods:{
-    coordenadas: function(){
-      return [this.ip.location.lat, this.ip.location.lng]
+  methods: {
+    coordenadas: function() {
+      return this.ip
+        ? [this.ip.location.lat, this.ip.location.lng]
+        : ["33.7819589", "-84.3894095"];
     }
   }
-}
+};
 </script>
-<style >
-
-.leaflet-pane .leaflet-marker-pane img{
-width: 2rem !important;
+<style>
+.leaflet-pane .leaflet-marker-pane img {
+  width: 2rem !important;
 }
 </style>
